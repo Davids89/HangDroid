e5ea60e1f218a62b21c798e225a0df6f5c156400
David Luque <dvluque@piggate.com>
2015-08-25 20:53:59 +0200

    Don't need random word

--- app/src/main/java/com/example/david/hangdroid/GameMultiActivity.java
+++ app/src/main/java/com/example/david/hangdroid/GameMultiActivity.java
@@ -22,9 +22,6 @@ public class GameMultiActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multi_game);
-        setRandomWord();
-
-        Log.d("Valor", getIntent().getStringExtra("WORD"));
     }
 
     /**
